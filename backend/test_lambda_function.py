59e0c4b79c979cab0f267b01aa1bc4fa3fd3eb2d
King Yiu Suen <kingyiusuen@gmail.com>
2022-01-06 17:57:26 -0600

    Fix unit tests

--- backend/test_lambda_function.py
+++ backend/test_lambda_function.py
@@ -2,7 +2,7 @@ from lambda_function import lambda_handler
 
 
 def test_valid_input():
-    event = {"text": "I love machine learning."}
+    event = {"body": {"text": "I love machine learning."}}
     response = lambda_handler(event, None)
     assert response["status_code"] == 200
     assert len(response["body"]["predictions"]) == 2
